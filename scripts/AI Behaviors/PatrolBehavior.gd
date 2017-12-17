cb6f1798f6966cf97b672bfce4533cb0fab559c6
vhlinh <vuhoanglinh1997@gmail.com>
2017-12-17 15:25:50 +0700

    Create new branch, change Architecture from inheritance to preload

--- scripts/AI Behaviors/PatrolBehavior.gd
+++ scripts/AI Behaviors/PatrolBehavior.gd
@@ -1,119 +1,62 @@
-extends RigidBody2D
-
-# onready var
-onready var flip        = get_node("flip")  # Character
-onready var ground_dt   = get_node("ground_detector") # Character
-onready var bound_dt_1  = get_node("bound_detector_1") # Character
-onready var bound_dt_2  = get_node("bound_detector_2") # Character
-onready var wall_dt     = get_node("flip/wall_detector") # Character
-onready var anim        = get_node("flip/Sprite/anim") # Character
-
-# Character
-# export var
-export var max_health         = 10
-export var extra_gravity      = 2500
-export var accerleration      = 100
-export var max_velocity       = 300
-export var jump_force         = 800
-export var max_time_to_walk   = 5
-export var max_time_btw_walks = 2
+extends Node
 
 # private var
-var speed          = Vector2()
-var direction      = 1
-var time           = 0
-var time_btw_walks = 0
-var time_to_walk   = 0
-var time_to_acc    = max_velocity / (accerleration * 100)
+var body
+var time
+var time_btw_walks
+var time_to_walk
+var time_to_acc
 
-# Character
-# stats
-var cur_health = 0
-
-func _ready():
-	set_process(true)
-	ground_dt.add_exception(self)
-	wall_dt.add_exception(self)
-	bound_dt_1.add_exception(self)
-	bound_dt_2.add_exception(self)
-	print("Add Exception Done!")
-	
-	# Character
-	set_applied_force(Vector2(0, extra_gravity))
-	cur_health = max_health
+func _init(body):
+	self.body = body
+	first_run()
 	pass
 
-# Character
-func _process(delta):
-	# flip the sprite
-	flip.set_scale(Vector2(direction, 1))
-	
-	# death
-	if cur_health <= 0:
-		queue_free()
-	pass
-
-# Character
-# Check for the ground
-func ground_check():
-	if ground_dt.is_colliding():
-		var body = ground_dt.get_collider()
-		if body.is_in_group("GROUND"):
-			return true
-	else:
-		return false
-	pass
-
-# Character
-func damaged(damage, direction, push_back_force, status):
-	cur_health -= damage
-	set_linear_velocity(Vector2(push_back_force.x*direction, push_back_force.y))
-	flip.set_scale(Vector2( direction , 1))
+func first_run():
+	time = 0
+	time_btw_walks = 0
+	time_to_walk = 0
+	time_to_acc  = body.max_velocity / (body.accerleration * 100)
 	pass
 
 ## BEHAVIOR SCRIPT
-func patrolBehavior(delta):
-	patrol(delta)
-	
-	if ground_check():
-		# Check for platform edges
-		if direction > 0 and not bound_dt_1.is_colliding():
-			direction = -direction
-		elif direction < 0 and not bound_dt_2.is_colliding():
-			direction = -direction
-		
-		# Check for vertical walls
-		if wall_dt.is_colliding():
-			var normal = wall_dt.get_collision_normal()
-			if sign(normal.x * direction) < 0:
-				direction = -direction
-	pass
-
 # PATROLING
 func patrol(delta):
-	time += delta
+	stepRandomly(delta)
+	
+	if body.ground_check():
+		# Check for platform edges
+		if body.direction > 0 and not body.bound_dt_1.is_colliding():
+			body.direction = -body.direction
+		elif body.direction < 0 and not body.bound_dt_2.is_colliding():
+			body.direction = -body.direction
+		
+		# Check for vertical walls
+		if body.wall_dt.is_colliding():
+			var normal = body.wall_dt.get_collision_normal()
+			if sign(normal.x * body.direction) < 0:
+				body.direction = -body.direction
+	pass
+
+func stepRandomly(delta):
+	time = time + delta
 	
 	if time > time_to_walk + time_btw_walks:
 		time = 0
 		randomize()
-		time_btw_walks = rand_range(time_to_acc, max_time_btw_walks)
-		time_to_walk   = rand_range(time_to_acc, max_time_to_walk)
+		time_btw_walks = rand_range(time_to_acc, body.max_time_btw_walks)
+		time_to_walk   = rand_range(time_to_acc, body.max_time_to_walk)
 		
 		# randomize the direction between -1 and 1
 		if round(randf()):
-			direction = 1
+			body.direction = 1
 		else:
-			direction = -1
+			body.direction = -1
 	
 	if time < time_to_walk:
-		speed.x = lerp(speed.x, max_velocity * direction, accerleration*0.01) #acc * 0.01, turn into percent
-		set_linear_velocity(Vector2( speed.x, get_linear_velocity().y ))
+		body.speed.x = lerp(body.speed.x, body.max_velocity * body.direction, body.accerleration*0.01) #acc * 0.01, turn into percent
+		body.set_linear_velocity(Vector2( body.speed.x, body.get_linear_velocity().y ))
 	else:
-		stopMovement()
-	pass
-
-# STOPPING
-func stopMovement():
-	speed.x = lerp(speed.x, 0, accerleration*0.01)
-	set_linear_velocity(Vector2( speed.x, get_linear_velocity().y ))
+		body.speed.x = lerp(body.speed.x, 0, body.accerleration*0.01)
+		body.set_linear_velocity(Vector2( body.speed.x, body.get_linear_velocity().y ))
 	pass
